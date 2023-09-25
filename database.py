57359af23671355f188997a2a1ba843f2396f1a7
tanzin706 <61379151+tanzin706@users.noreply.github.com>
2023-09-25 05:53:14 +0000

    Applications saved into Database

--- database.py
+++ database.py
@@ -29,3 +29,22 @@ def load_job_from_db(id):
       return
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "Insert into applications (job_id, full_name, email, linkedin_url, education, Work_experience, resume_url)Values(:job_id, :full_name, :email, :linkedin_url, :education, :Work_experience, :resume_url)"
+    )
+
+    all_data = {
+      'job_id': job_id,
+      'full_name': data['full_name'],
+      'email': data['email'],
+      'linkedin_url': data['linkedid'],
+      'education': data['education'],
+      'Work_experience': data['Work_experience'],
+      'resume_url': data['resume_url']
+    }
+
+    conn.execute(query, all_data)
